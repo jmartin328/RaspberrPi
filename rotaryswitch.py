92a5231c4f1c8835649eb6a5ca9aeedfbfad43f0
jmartin3 <jmartin3@iastate.edu>
2020-01-21 22:52:33 -0600

    work on rotaryswitch logic

--- rotaryswitch.py
+++ rotaryswitch.py
@@ -7,7 +7,11 @@ GPIO.setmode(GPIO.BOARD)
 #Initialize pygame mixer
 mixer.init()
 
+mixer.music.unload()
 mixer.music.load('/home/pi/Desktop/Tracks/Emaj.wav')
+
+current = "Emaj"
+new = "null"
 
 # assign pins to keys
 C = 13
@@ -25,29 +29,50 @@ Bb = 37
 
 #power
 GPIO.setup(7, GPIO.OUT)
+GPIO.output(7, True)
+
 #on/off
 GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-#selector
+
+#switch output
 GPIO.setup(13, GPIO.IN)
 GPIO.setup(15, GPIO.IN)
+# GPIO.setup(16, GPIO.IN)
+# GPIO.setup(18, GPIO.IN)
+# GPIO.setup(22, GPIO.IN)
+# GPIO.setup(29, GPIO.IN)
+# GPIO.setup(31, GPIO.IN)
+# GPIO.setup(32, GPIO.IN)
+# GPIO.setup(33, GPIO.IN)
+# GPIO.setup(36, GPIO.IN)
+# GPIO.setup(37, GPIO.IN)
+# GPIO.setup(, GPIO.IN)
 
 def handle(pin):
-    #assign sound to the active pin
-    if (pin == C):
+	#update currently playing
+	#unload, and load new music
+    if (pin == Emaj and current != pin):
+		new = pin
+		mixer.music.unload()
         mixer.music.load('/home/pi/Desktop/Tracks/Emaj.wav')
-    elif (pin == G):
+    elif (pin == Emin and current != pin):
+		new = pin
+		mixer.music.unload()
         mixer.music.load('/home/pi/Desktop/Tracks/Emin.wav')
+	#print the pin that is being handled
     print(pin)
 
 	
-GPIO.add_event_detect(C, GPIO.BOTH, callback=handle, bouncetime=3)	
-GPIO.add_event_detect(G, GPIO.BOTH, callback=handle, bouncetime=3)
+GPIO.add_event_detect("Emaj", GPIO.RISING, callback=handle, bouncetime=3000)	
+GPIO.add_event_detect("Emin", GPIO.RISING, callback=handle, bouncetime=3000)
 
 
 try:
     while True:
-        GPIO.output(7, True)
-        mixer.music.play(-1)
+		#if there is a different pin active, start player
+		if current != new:
+			current = new
+			mixer.music.play()
 finally:     
     GPIO.cleanup()
 	
