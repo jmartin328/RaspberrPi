f1b5fc3a57c0d605d13903d6a31747e3c2a50722
jmartin3 <jmartin3@iastate.edu>
2020-05-25 22:12:37 -0500

    repeating and different root/lengths added

--- melodyplayer.py
+++ melodyplayer.py
@@ -6,22 +6,32 @@ from numpy.random import seed
 from numpy.random import shuffle
 from numpy.random import randint
 
+def get_input():
+	text = input("Repeat? : ")
+	if (text == "y"):
+		return 1
+	else:
+		return 0
 
-def generate_melody():
+
+def generate_melody(length, root):
 	#seed the randome number generator
 	seed()
 
-	#prepare a major scale sequence
-	base = 60 #C4
-	degrees = [base+0, base+2, base+4, base+5, base+7, base+9, base+11, base+12]
+	base = int(root)
+	len = int(length)
+	#prepare a major scale array based off the root, C4 is 60
+	notes = [base+0, base+2, base+4, base+5, base+7, base+9, base+11, base+12]
 
-	#shuffle the sequence
-	shuffle(degrees)
+	degrees = []
+	#generate a sequence of the length
+	for i in range(0, len):
+		degrees.append(notes[randint(0,7)])
 
 	track    = 0
 	channel  = 0
 	time     = 0 #in beats
-	duration = randint(1,4,8) #in beats 
+	duration = randint(1,4,len) #in beats 
 	tempo    = 100 #in BPM
 	volume   = 100 #0-127, as per the MIDI standard
 
@@ -41,7 +51,6 @@ def play_melody(music):
 	clock = pygame.time.Clock()
 	try:
 		pygame.mixer.music.load(music)
-		print ("Midi file loaded!")
 	except pygame.error:
 		print ("File not found")
 		return
@@ -59,8 +68,23 @@ pygame.mixer.init(freq, bitsize, channels, buffer)
 
 pygame.mixer.music.set_volume(0.8)
 try:
-	generate_melody()
-	play_melody(midi_file)
+	#infinite loop 
+	while (1):
+		#get the length
+		len = input ("Enter a length : ")
+		#get the root
+		roo = input ("Enter a root : ")
+		#generate a new melody
+		generate_melody(len, roo)
+		#play the new melody for the first time
+		play_melody(midi_file)
+		#find if they want a repeat
+		flag = get_input()
+		#while they still want a repeat, keep playing the same melody
+		while (flag == 1):
+			play_melody(midi_file)
+			flag = get_input()
+
 except KeyboardInterrupt:
 	pygame.mixer.music.fadeout(1000)
 	pygame.mixer.music.stop()
